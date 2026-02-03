4c5aaf1389cc3b9ad001bd3ef479d7f23bdfa960
Guna1237 <gunaratnam1987@gmail.com>
2026-02-03 22:26:13 +0530

    Fix login content-type for OAuth2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -25,7 +25,11 @@ export const login = async (username, password) => {
     const params = new URLSearchParams();
     params.append('username', username);
     params.append('password', password);
-    const response = await default_api.post('/token', params);
+    const response = await default_api.post('/token', params, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
     if (response.data.access_token) {
         localStorage.setItem('token', response.data.access_token);
     }
